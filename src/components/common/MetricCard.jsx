fe0ba2790bf55f61d7117cc6a6828294ed5c1298
박윤희 <dbsgml0379@naver.com>
2026-09-14 16:23:45 +0900

    chore: remove legacy frontend files

--- src/components/common/MetricCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function MetricCard({ label, value, sub }) {
-  return (
-    <div className="metric-card">
-      <span>{label}</span>
-      <strong>{value}</strong>
-      {sub && <small>{sub}</small>}
-    </div>
-  );
-}
